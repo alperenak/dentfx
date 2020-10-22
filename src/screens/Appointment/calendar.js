7c3ce4835bdc14bec5a1b3fea82bcc16cca547a5
ikamaci <ibrahim.kamaci@boun.edu.tr>
2020-10-22 08:47:35 +0300

    modify mock calendar events

--- src/screens/Appointment/calendar.js
+++ src/screens/Appointment/calendar.js
@@ -26,168 +26,90 @@ class ACalendar extends Component {
 	render() {
 		let { activeLink } = this.state;
 		const localizer = momentLocalizer(moment)
+        let events = [
+        {
+          id: 11,
+          title: 'Appointment - Gustavo Velasco',
+          start: new Date(2020, 9, 13, 7, 0, 0),
+          end: new Date(2020, 9, 13, 7, 30, 0),
+        },
+        {
+          id: 12,
+          title: 'Appointment - Chad Kelly',
+          start: new Date(2020, 9, 17, 11, 30, 0),
+          end: new Date(2020, 9, 17, 12, 0, 0),
+        },
+        {
+          id: 12.5,
+          title: 'Appointment - Erhan Temiz',
+          start: new Date(2020, 9, 22, 8, 30, 0),
+          end: new Date(2020, 9, 22, 9, 0, 0),
+        },
+        {
+          id: 10,
+          title: 'Appointment - Alfredo Benz',
+          start: new Date(2020, 9, 22, 9, 0, 0),
+          end: new Date(2020, 9, 22, 9, 30, 0),
+        },
+        {
+          id: 13,
+          title: 'Busy',
+          start: new Date(2020, 9, 20, 13, 30, 0),
+          end: new Date(2020, 9, 21, 16, 0, 0),
+        },
+        {
+          id: 14,
+          title: 'Appointment - Richard Hailey',
+          start: new Date(new Date().setHours(new Date().getHours() - 3)),
+          end: new Date(new Date().setHours(new Date().getHours() + 3)),
+        },
+
+        {
+          id: 16,
+          title: 'Appointment - James Fox',
+          start: new Date(2020, 9, 14, 15, 30, 0),
+          end: new Date(2020, 9, 14, 19, 0, 0),
+        },
+        {
+          id: 17,
+          title: 'Appointment - Gustavo Velasco',
+          start: new Date(2020, 10, 14, 16, 30, 0),
+          end: new Date(2020, 10, 14, 20, 0, 0),
+        },
+        {
+          id: 18,
+          title: 'Appointment - Chad Kelly',
+          start: new Date(2020, 10, 14, 16, 30, 0),
+          end: new Date(2020, 10, 14, 17, 30, 0),
+        },
+        {
+          id: 19,
+          title: 'Appointment - Alfredo Bale',
+          start: new Date(2020, 9, 14, 17, 30, 0),
+          end: new Date(2020, 9, 14, 20, 30, 0),
+        },
+        {
+          id: 20,
+          title: 'Appointment - Alfredo Benz',
+          start: new Date(2020, 10, 14, 17, 0, 0),
+          end: new Date(2020,10, 14, 18, 30, 0),
+        },
+        {
+          id: 21,
+          title: 'Busy',
+          start: new Date(2020, 10, 14, 17, 0, 0),
+          end: new Date(2020, 10, 14, 18, 30, 0),
+        }
+      ];
 	return (
 		<div>
 		<Calendar
   			localizer={localizer}
-  			events={[
-  {
-    id: 0,
-    title: 'All Day Event very long title',
-    allDay: true,
-    start: new Date(2020, 3, 0),
-    end: new Date(2020, 3, 1),
-  },
-  {
-    id: 1,
-    title: 'Long Event',
-    start: new Date(2020, 3, 7),
-    end: new Date(2020, 3, 10),
-  },
-
-  {
-    id: 2,
-    title: 'DTS STARTS',
-    start: new Date(2020, 2, 13, 0, 0, 0),
-    end: new Date(2020, 2, 20, 0, 0, 0),
-  },
-
-  {
-    id: 3,
-    title: 'DTS ENDS',
-    start: new Date(2020, 10, 6, 0, 0, 0),
-    end: new Date(2020, 10, 13, 0, 0, 0),
-  },
-
-  {
-    id: 4,
-    title: 'Some Event',
-    start: new Date(2020, 3, 9, 0, 0, 0),
-    end: new Date(2020, 3, 10, 0, 0, 0),
-  },
-  {
-    id: 5,
-    title: 'Conference',
-    start: new Date(2020, 3, 11),
-    end: new Date(2020, 3, 13),
-    desc: 'Big conference for important people',
-  },
-  {
-    id: 6,
-    title: 'Meeting',
-    start: new Date(2020, 3, 12, 10, 30, 0, 0),
-    end: new Date(2020, 3, 12, 12, 30, 0, 0),
-    desc: 'Pre-meeting meeting, to prepare for the meeting',
-  },
-  {
-    id: 7,
-    title: 'Lunch',
-    start: new Date(2020, 3, 12, 12, 0, 0, 0),
-    end: new Date(2020, 3, 12, 13, 0, 0, 0),
-    desc: 'Power lunch',
-  },
-  {
-    id: 8,
-    title: 'Meeting',
-    start: new Date(2020, 3, 12, 14, 0, 0, 0),
-    end: new Date(2020, 3, 12, 15, 0, 0, 0),
-  },
-  {
-    id: 9,
-    title: 'Happy Hour',
-    start: new Date(2020, 3, 12, 17, 0, 0, 0),
-    end: new Date(2020, 3, 12, 17, 30, 0, 0),
-    desc: 'Most important meal of the day',
-  },
-  {
-    id: 10,
-    title: 'Dinner',
-    start: new Date(2020, 3, 12, 20, 0, 0, 0),
-    end: new Date(2020, 3, 12, 21, 0, 0, 0),
-  },
-  {
-    id: 11,
-    title: 'Birthday Party',
-    start: new Date(2020, 10, 13, 7, 0, 0),
-    end: new Date(2020, 10, 13, 10, 30, 0),
-  },
-  {
-    id: 12,
-    title: 'Late Night Event',
-    start: new Date(2020, 10, 17, 19, 30, 0),
-    end: new Date(2020, 10, 18, 2, 0, 0),
-  },
-  {
-    id: 12.5,
-    title: 'Late Same Night Event',
-    start: new Date(2020, 10, 17, 19, 30, 0),
-    end: new Date(2020, 10, 17, 23, 30, 0),
-  },
-  {
-    id: 13,
-    title: 'Multi-day Event',
-    start: new Date(2020, 10, 20, 19, 30, 0),
-    end: new Date(2020, 10, 22, 2, 0, 0),
-  },
-  {
-    id: 14,
-    title: 'Today',
-    start: new Date(new Date().setHours(new Date().getHours() - 3)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3)),
-  },
-
-  {
-    id: 16,
-    title: 'Video Record',
-    start: new Date(2020, 11, 14, 15, 30, 0),
-    end: new Date(2020, 11, 14, 19, 0, 0),
-  },
-  {
-    id: 17,
-    title: 'Dutch Song Producing',
-    start: new Date(2020, 10, 14, 16, 30, 0),
-    end: new Date(2020, 10, 14, 20, 0, 0),
-  },
-  {
-    id: 18,
-    title: 'Itaewon Halloween Meeting',
-    start: new Date(2020, 10, 14, 16, 30, 0),
-    end: new Date(2020, 10, 14, 17, 30, 0),
-  },
-  {
-    id: 19,
-    title: 'Online Coding Test',
-    start: new Date(2020, 10, 14, 17, 30, 0),
-    end: new Date(2020, 10, 14, 20, 30, 0),
-  },
-  {
-    id: 20,
-    title: 'An overlapped Event',
-    start: new Date(2020, 10, 14, 17, 0, 0),
-    end: new Date(2020,10, 14, 18, 30, 0),
-  },
-  {
-    id: 21,
-    title: 'Phone Interview',
-    start: new Date(2020, 10, 14, 17, 0, 0),
-    end: new Date(2020, 10, 14, 18, 30, 0),
-  },
-  {
-    id: 22,
-    title: 'Cooking Class',
-    start: new Date(2020, 10, 14, 17, 30, 0),
-    end: new Date(2020, 10, 14, 19, 0, 0),
-  },
-  {
-    id: 23,
-    title: 'Go to the gym',
-    start: new Date(2020, 10, 14, 18, 30, 0),
-    end: new Date(2020, 10, 14, 20, 0, 0),
-  },
-]}
+  			events= {events}
   			startAccessor="start"
   			endAccessor="end"
-  			style={{ height: 500 }}
+  			style={{ height: 500,
+            }}
 		/>
 		</div>
 	);
